cdcc017e8f516bfc8c1a1b13c3cc9bab31dd31b7
Aumin Patel <apatel@alpha-sense.com>
2022-09-13 18:23:15 +0530

    chore: zoid-demo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+window.MyLoginZoidComponent = zoid.create({
+  dimensions: {
+    width: "900px",
+    height: "900px",
+  },
+
+  // The html tag used to render my component
+
+  tag: "my-login-component",
+
+  // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+
+  url: 'https://landing-page-2-git-zoid-demo-auminpatel.vercel.app/',
+});
